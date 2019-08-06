c3094b3a28a71cf34f426ef02f09194a89ef5235
0918nobita <nobita.0918@gmail.com>
2019-08-06 09:36:04 +0900

    :art: Improve readability

--- compiler/malloc_func.ml
+++ compiler/malloc_func.ml
@@ -5,12 +5,13 @@ let init =
   Func
     { signature = { params = 0; results = 0}
     ; locals = 0
-    ; code = bin_of_wasm_insts
-      [ I32_store ([I32_const 0], [I32_const 8])
-      ; I32_store ([I32_const 4], [I32_const 0])
-      ; I32_store ([I32_const 8], [I32_const 0])
-      ; I32_store ([I32_const 12], [I32_const 43978])
-      ]
+    ; code =
+      bin_of_wasm_insts
+        [ I32_store ([I32_const 0], [I32_const 8])
+        ; I32_store ([I32_const 4], [I32_const 0])
+        ; I32_store ([I32_const 8], [I32_const 0])
+        ; I32_store ([I32_const 12], [I32_const 43978])
+        ]
     }
 
 let malloc =
@@ -24,48 +25,53 @@ let free =
   Func
     { signature = { params = 1; results = 0}
     ; locals = 3
-    ; code = bin_of_wasm_insts
-      [ Set_local (1, [I32_sub ([Get_local 0], [I32_const 8])])
-      ; Block Void
-        ; Loop Void
-          ; Set_local (2, [I32_load [Get_local 2]])
-          ; If
-            { ret_type = Void
-            ; cond = [I32_gt ([Get_local 2], [Get_local 1])]
-            ; then_ =
-              [ I32_store ([Get_local 3], [Get_local 1])
-              ; If_else
-                { ret_type = Void
-                ; cond =
-                  [I32_eq
-                    ( [Get_local 2]
-                    , [I32_add
-                        ( [I32_add (
-                            [Get_local 1],
-                            [I32_load [I32_add ([Get_local 1], [I32_const 4])]])]
-                        , [I32_const 8]
-                        )]
-                    )
-                  ]
-                ; then_ =
-                  [ I32_store ([Get_local 1], [I32_load [Get_local 2]])
-                  ; I32_store
-                    ( [I32_add ([Get_local 1], [I32_const 4])]
-                    , [I32_add
-                        ( [I32_add ([I32_const 8], [I32_load [I32_add ([Get_local 1], [I32_const 4])]])]
-                        , [I32_load [I32_add ([Get_local 2], [I32_const 4])]]
-                        )]
-                    )
-                  ]
-                ; else_ = [I32_store ([Get_local 1], [Get_local 2])]
-                }
-              ; Return
-              ]
-            }
-          ; Set_local (3, [Get_local 2])
-          ; Br_if (0, [I32_ne ([I32_load [Get_local 2]], [I32_const 0])])
+    ; code =
+      let ptr = 0 in
+      let target = 1 in
+      let current = 2 in
+      let previous = 3 in
+      bin_of_wasm_insts
+        [ Set_local (target, [I32_sub ([Get_local ptr], [I32_const 8])])
+        ; Block Void
+          ; Loop Void
+            ; Set_local (current, [I32_load [Get_local current]])
+            ; If
+              { ret_type = Void
+              ; cond = [I32_gt ([Get_local current], [Get_local target])]
+              ; then_ =
+                [ I32_store ([Get_local previous], [Get_local target])
+                ; If_else
+                  { ret_type = Void
+                  ; cond =
+                    [I32_eq
+                      ( [Get_local current]
+                      , [I32_add
+                          ( [I32_add (
+                              [Get_local target],
+                              [I32_load [I32_add ([Get_local target], [I32_const 4])]])]
+                          , [I32_const 8]
+                          )]
+                      )
+                    ]
+                  ; then_ =
+                    [ I32_store ([Get_local target], [I32_load [Get_local current]])
+                    ; I32_store
+                      ( [I32_add ([Get_local target], [I32_const 4])]
+                      , [I32_add
+                          ( [I32_add ([I32_const 8], [I32_load [I32_add ([Get_local target], [I32_const 4])]])]
+                          , [I32_load [I32_add ([Get_local current], [I32_const 4])]]
+                          )]
+                      )
+                    ]
+                  ; else_ = [I32_store ([Get_local target], [Get_local current])]
+                  }
+                ; Return
+                ]
+              }
+            ; Set_local (previous, [Get_local current])
+            ; Br_if (0, [I32_ne ([I32_load [Get_local current]], [I32_const 0])])
+          ; End
+          ; I32_store ([Get_local previous], [Get_local target])
         ; End
-        ; I32_store ([Get_local 3], [Get_local 1])
-      ; End
-      ]
-    }
+        ]
+      }
--- compiler/stack_func.ml
+++ compiler/stack_func.ml
@@ -1,29 +1,36 @@
 open Wasm
 open Wast
 
+let sp = 0 (* stack pointer *)
+
 let push =
   Func
     { signature = { params = 1; results = 0}
     ; locals = 0
-    ; code = bin_of_wasm_insts
-      [ I32_store ([Get_global 0], [Get_local 0])
-      ; Set_global (0, [I32_sub ([Get_global 0], [I32_const 4])])
-      ]
+    ; code =
+      let value = 0 in
+      bin_of_wasm_insts
+        [ I32_store ([Get_global sp], [Get_local value])
+        ; Set_global (sp, [I32_sub ([Get_global sp], [I32_const 4])])
+        ]
     }
 
 let pop_func =
   Func
     { signature = { params = 0; results = 1}
     ; locals = 0
-    ; code = bin_of_wasm_insts
-      [ Set_global (0, [I32_add ([Get_global 0], [I32_const 4])])
-      ; I32_load [Get_global 0]
-      ]
+    ; code =
+      bin_of_wasm_insts
+        [ Set_global (sp, [I32_add ([Get_global sp], [I32_const 4])])
+        ; I32_load [Get_global sp]
+        ]
     }
 
 let top =
   Func
     { signature = { params = 0; results = 1}
     ; locals = 0
-    ; code = bin_of_wasm_insts [ I32_load [I32_add ([Get_global 0], [I32_const 4])] ]
+    ; code =
+      bin_of_wasm_insts
+        [ I32_load [I32_add ([Get_global sp], [I32_const 4])] ]
     }
