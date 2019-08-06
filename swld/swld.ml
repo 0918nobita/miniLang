e630d418154ab3199ec8b29899101b1755d072da
0918nobita <nobita.0918@gmail.com>
2019-08-07 00:06:10 +0900

    :construction: アルファベット1文字のパーサを追加した

--- swld/swld.ml
+++ swld/swld.ml
@@ -2,6 +2,10 @@ open Parser_combinator
 
 let digit_char = oneOf "0123456789"
 
+let letter = satisfy (fun (_, c) ->
+  let code = Char.code c in
+    (65 <= code && code <= 90) || (97 <= code && code <= 122))
+
 let version = "0.0.2"
 
 let () =
