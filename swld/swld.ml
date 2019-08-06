8b387c59258d8e9f23a25a1548c26a08f763d236
0918nobita <nobita.0918@gmail.com>
2019-08-07 00:03:23 +0900

    :construction: swld で、引数を指定しなかったときにバージョンを出力するようにした

--- swld/swld.ml
+++ swld/swld.ml
@@ -1,4 +1,13 @@
 open Parser_combinator
 
+let version = "0.0.2"
+
 let () =
-  print_endline "SWLD - Segmental WASM Linker"
+  if Array.length Sys.argv = 1
+    then
+      begin
+        print_endline "SWLD - Segmental WASM Linker";
+        print_endline @@ "Version " ^ version
+      end
+    else
+      ()
