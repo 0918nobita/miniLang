41d0f1e6ae2e09c9a1f3b514d066d9b025a7459b
0918nobita <nobita.0918@gmail.com>
2019-09-18 17:27:09 +0900

    :construction: 複雑な AST を文字列に変換して出力するサンプルを追加した

--- compiler/src/App/Program.fs
+++ compiler/src/App/Program.fs
@@ -2,18 +2,22 @@
     | IntLiteral of value : int
     | Add of lhs : Expr * rhs : Expr
 
-let rec showExpr = function
+let rec showExpr =
+    function
     | IntLiteral(v) ->
-        string(v)
+        string (v)
     | Add(lhs, rhs) ->
         "Add(" + (showExpr lhs) + ", " + (showExpr rhs) + ")"
 
 [<EntryPoint>]
 let main argv =
-    Add(
-        lhs = IntLiteral(value = 3),
-        rhs = IntLiteral(value = 4)
-    )
+    Add
+        (lhs = 
+            Add
+                (lhs = IntLiteral(value = 1),
+                 rhs = IntLiteral (value = 2)),
+         rhs =
+            IntLiteral (value = 4))
     |> showExpr
     |> printfn "%s"
     0
