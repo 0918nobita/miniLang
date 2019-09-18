7292b507a52021d62a0d213e5c617697f6ce596a
0918nobita <nobita.0918@gmail.com>
2019-09-18 16:54:56 +0900

    :construction: 式の AST を表す判別共用体を定義した

--- compiler/src/App/Program.fs
+++ compiler/src/App/Program.fs
@@ -1,4 +1,19 @@
-﻿[<EntryPoint>]
+﻿type Expr =
+    | IntLiteral of value : int
+    | Add of lhs : Expr * rhs : Expr
+
+let rec showExpr = function
+    | IntLiteral(v) ->
+        string(v)
+    | Add(lhs, rhs) ->
+        "Add(" + (showExpr lhs) + ", " + (showExpr rhs) + ")"
+
+[<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Add(
+        lhs = IntLiteral(value = 3),
+        rhs = IntLiteral(value = 4)
+    )
+    |> showExpr
+    |> printfn "%s"
     0
