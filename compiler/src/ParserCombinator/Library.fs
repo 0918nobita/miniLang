e71da8045546778651cb2564bd79cee1a6ceae7f
0918nobita <nobita.0918@gmail.com>
2019-09-19 17:57:46 +0900

    :sparkles: ParserCombinator プロジェクトを追加した

---
+++ compiler/src/ParserCombinator/Library.fs
@@ -0,0 +1,8 @@
+﻿module ParserCombinator
+
+type Location =
+    { line : int; chr : int }
+    override m.ToString () =
+        string(m.line + 1) + ":" + string(m.chr + 1)
+
+let bof = { line = 0; chr = 0 }
