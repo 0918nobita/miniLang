e364ba5fa82b9e3c6b482f02f3602fde6033b37a
0918nobita <nobita.0918@gmail.com>
2019-09-19 18:14:55 +0900

    :sparkles: + 演算子で Location 同士の合成ができるようにした
    左辺値をベース、右辺値を差分として合成する

--- compiler/src/ParserCombinator/Library.fs
+++ compiler/src/ParserCombinator/Library.fs
@@ -4,5 +4,10 @@ type Location =
     { line : int; chr : int }
     override m.ToString () =
         string(m.line + 1) + ":" + string(m.chr + 1)
+    static member (+) (lhs : Location, rhs : Location) =
+        {
+            line = lhs.line + rhs.line
+            chr = if rhs.line >= 1 then rhs.chr else lhs.chr + rhs.chr
+        }
 
 let bof = { line = 0; chr = 0 }
