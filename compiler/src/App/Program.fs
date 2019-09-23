f723aec306e18ae0861943061f67742d150fe8be
0918nobita <nobita.0918@gmail.com>
2019-09-23 23:30:54 +0900

    :sparkles: printfn "%A" で AST を標準出力するサンプルを追加した

--- compiler/src/App/Program.fs
+++ compiler/src/App/Program.fs
@@ -1,61 +1,13 @@
 ﻿open ParserCombinator
 
 type Expr =
-    | IntLiteral of value : int
-    | Add of lhs : Expr * rhs : Expr
-
-let rec showExpr =
-    function
-    | IntLiteral(v) ->
-        string (v)
-    | Add(lhs, rhs) ->
-        "Add(" + (showExpr lhs) + ", " + (showExpr rhs) + ")"
-
-type ZeroBuilder() =
-    member __.Zero() =
-        printfn "computation expressions!"
-
-let zero = ZeroBuilder()
-
-// ビルダー式に関係のない式のみを記述した場合には、
-// Zero メソッドの呼び出しが挿入される
-zero { () }
-zero { printfn "Hello" }
-zero { printf "Hello "; printfn "F#" }
-
-// builder-expr { cexpr } は
-//   let b = builder-expr in
-//   b.Run(<@ b.Delay(fun () -> cexpr) @>)
-// のように展開される
-
-open FSharp.Quotations
-
-type SampleBuilder() =
-    member __.Zero() = ()
-    member __.Quote() = ()
-    member __.Run(expr: Expr<_>) =
-        printfn "%A" expr
-
-let s = SampleBuilder()
-
-s { printfn "HELLO" }
-
-type OptionBuilder() =
-    member __.Return(x) = Some x
-    member __.ReturnFrom(x: _ option) = x
-    member __.Bind(x, f) =
-        Option.bind f x
-
-let opt = OptionBuilder()
-
-printfn "%A" <| opt { return 0 }  // opt.Return(0)
-printfn "%A" <| opt { return! Some 27 }  // opt.ReturnFrom(Some 27)
-
-printfn "%A" <| opt { let! a = Some 1 in return a }
-// opt.Bind(Some 1, fun a -> option.Return(a))
+    | IntLiteral of loc : Location * value : int
+    | Add of loc : Location * lhs : Expr * rhs : Expr
+    | Sub of loc : Location * lhs : Expr * rhs : Expr
 
 [<EntryPoint>]
 let main argv =
+    printfn "%A" <| IntLiteral(bof, 8)
     if Array.isEmpty argv
         then -1
         else
