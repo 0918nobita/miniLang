018de23e415381c51ca5e1d25c54ba8b4ed53749
0918nobita <nobita.0918@gmail.com>
2019-09-26 15:40:02 +0900

    :sparkles: 単項演算子と自然数リテラルのパーサを追加した

--- compiler/src/App/Program.fs
+++ compiler/src/App/Program.fs
@@ -1,10 +1,30 @@
 ﻿open System
 open ParserCombinator
 
+type ExprAST =
+    | IntLiteral of loc : Location * value : int
+    | Minus of loc : Location * expr : ExprAST
+
+let zero = char '0'
+
+let nonZeroDigit = oneOf "123456789"
+
+let digit = zero <|> nonZeroDigit
+
+let unary =
+    let plus = char '+' |. succeed id
+    let minus = char '-' |= (fun (loc, _) -> succeed (fun ast -> Minus (loc, ast)))
+    plus <|> minus <|> succeed id
+
+let nat =
+    let digitVal = fmap (fun (loc, c) -> (loc, int c - int '0')) digit
+    let toNum x acc = x * 10 + acc
+    some digitVal
+    |= (fun result ->
+        succeed <| IntLiteral (fst (List.head result), List.fold toNum 0 (List.map snd result)))
+
 let letter =
     oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-let digit = oneOf "0123456789"
 
 let identifier =
     letter
@@ -23,12 +43,12 @@ let main argv =
         then -1
         else
             try
-                let result = parse identifier (bof, argv.[0])
+                let result = parse (unary <*> nat) (bof, argv.[0])
 
                 match result with
                 | Some { ast = ast; currentLoc = loc; rest = rest } ->
                     printfn "Success!"
-                    printfn "ast: %A" <| snd ast
+                    printfn "ast: %A" <| ast
                     printfn "currentLoc: %s" <| loc.ToString ()
                     printfn "rest: \"%s\"" rest
                     0
