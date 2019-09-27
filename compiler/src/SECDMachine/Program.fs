7b60e1bb412d1eedb1ecbef70c8b26f00f323d5a
0918nobita <nobita.0918@gmail.com>
2019-09-27 22:04:28 +0900

    :sparkles: SECD 仮想マシンを F# で実装するプロジェクトを追加した

---
+++ compiler/src/SECDMachine/Program.fs
@@ -0,0 +1,33 @@
+﻿open System.Collections.Generic
+
+let mem = Array.init 100000 (fun _ -> 0)
+
+let mutable pc = 0
+
+let mutable sp = 99999
+
+let push n =
+    mem.[sp] <- n
+    sp <- sp - 1
+
+let pop () =
+    sp <- sp + 1
+    mem.[sp]
+
+let env = Stack<Dictionary<string, int>>()
+
+let run () =
+    let mutable breakNow = false
+    while not breakNow do
+        let opcode = mem.[pc]
+        match opcode with
+        | 0 ->
+            printfn "STOP"
+            breakNow <- true
+        | _ ->
+            failwith "unknown opcode"
+
+[<EntryPoint>]
+let main _ =
+    run ()
+    0
