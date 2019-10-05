9b662006f2f31c816754ebd4d6284c2bce101e4d
0918nobita <nobita.0918@gmail.com>
2019-10-05 17:41:41 +0900

    :building_construction: ヘルプを表示したときには終了コードが 0 になるようにした

--- compiler/src/psyc/Program.fs
+++ compiler/src/psyc/Program.fs
@@ -15,10 +15,13 @@ type RecordType = { value: int }
 let main argv =
     let json = Json.deserialize<RecordType> "{ \"value\": 42 }"
     printfn "%A" json
-    try
-        let parser = ArgumentParser.Create<CLIArguments> "psyc"
-        printfn "%A" <| parser.Parse argv
-    with
-        | :? ArguParseException as exn ->
-            printfn "%s" <| exn.Message
+    let parser = ArgumentParser.Create<CLIArguments> "psyc"
+    let args =
+        try
+            parser.Parse argv
+        with
+            | :? ArguParseException as exn ->
+                printfn "%s" <| exn.Message
+                exit <| if exn.ErrorCode = ErrorCode.HelpText then 0 else 1
+    printfn "%A" args
     0
