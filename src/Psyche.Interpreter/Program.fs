ce4b3251975803c49f1527aa8672f75bcf53842b
0918nobita <nobita.0918@gmail.com>
2019-11-01 18:58:41 +0900

    :sparkles: コンピュテーション式でパーサを生成できるようにした

--- src/Psyche.Interpreter/Program.fs
+++ src/Psyche.Interpreter/Program.fs
@@ -1,18 +1,33 @@
+open ParserCombinator
+
 type MaybeBuilder () =
     member this.Bind (x, f) =
-        printfn "Bind: %A" x
         match x with
             | Some(x) -> f x
             | _ -> None
-    member this.Delay (f) =
-        printfn "Delay"
-        f ()
-    member this.Return (x) =
-        printfn "Return: %A" x
-        Some x
+    member this.Delay (f) = f ()
+    member this.Return (x) = Some x
+
+type MParsecBuilder () =
+    member this.Bind (p, f) =
+        {
+            parse = fun input ->
+                match parse p input with
+                | Some { ast = ast; currentLoc = loc; rest = rest } ->
+                    parse (f ast) (loc, rest)
+                | None -> None
+            error = None
+        }
+    member this.Return (ast) =
+        {
+            parse = fun (loc, rest) -> Some { ast = ast; currentLoc = loc; rest = rest }
+            error = None
+        }
 
 [<EntryPoint>]
-let main _ =
+let main argv =
+    if Array.isEmpty argv then exit(1)
+
     let maybe = MaybeBuilder()
     maybe {
         let x = 11
@@ -21,4 +36,15 @@ let main _ =
         return x + y + z
     }
     |> printfn "%A"
+
+    // "ac" または "bc" を受理して、１文字目と "!" を結合した文字列を返すパーサ
+    let parser = MParsecBuilder()
+    parser {
+        let! (_, first) = token "a" <|> token "b"
+        do! drop <| token "c"
+        return first + "!"
+    }
+    |> (fun p -> parse p (bof, argv.[0]))
+    |> printfn "%A"
+
     0
