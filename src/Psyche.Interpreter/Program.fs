f4d8f901f7d3f724e31addbaf28e203b20148efd
0918nobita <nobita.0918@gmail.com>
2019-11-01 16:52:11 +0900

    :sparkles: コンピュテーション式を用いたサンプルを追加

--- src/Psyche.Interpreter/Program.fs
+++ src/Psyche.Interpreter/Program.fs
@@ -1,4 +1,24 @@
+type MaybeBuilder () =
+    member this.Bind (x, f) =
+        printfn "Bind: %A" x
+        match x with
+            | Some(x) -> f x
+            | _ -> None
+    member this.Delay (f) =
+        printfn "Delay"
+        f ()
+    member this.Return (x) =
+        printfn "Return: %A" x
+        Some x
+
 [<EntryPoint>]
-let main argv =
-    printfn "Psyche Interpreter"
+let main _ =
+    let maybe = MaybeBuilder()
+    maybe {
+        let x = 11
+        let! y = Some 22
+        let! z = Some 33
+        return x + y + z
+    }
+    |> printfn "%A"
     0
