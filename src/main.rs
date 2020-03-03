aaa33f552f65a6e6d5d47502885256486cde7c11
0918nobita <nobita.0918@gmail.com>
2020-03-03 21:35:45 +0900

    :sparkles: function セクションを出力するようにした

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ extern crate serde_derive;
 
 use std::fs;
 
+mod func_section;
 mod header;
 mod import_section;
 mod type_section;
 
+use func_section::FuncSection;
 use header::Header;
 use import_section::ImportSection;
 use type_section::TypeSection;
@@ -19,13 +21,16 @@ fn main() {
     let header: Header = Default::default();
     let type_section: TypeSection = Default::default();
     let import_section: ImportSection = Default::default();
+    let func_section: FuncSection = Default::default();
 
     let mut encoded_header = bincode::serialize(&header).unwrap();
     let mut encoded_type_section = bincode::serialize(&type_section).unwrap();
     let mut encoded_import_section = bincode::serialize(&import_section).unwrap();
+    let mut encoded_func_section = func_section.serialize();
 
     encoded_header.append(&mut encoded_type_section);
     encoded_header.append(&mut encoded_import_section);
+    encoded_header.append(&mut encoded_func_section);
 
     fs::write("out.wasm", encoded_header).unwrap_or_else(|_| panic!("Failed to write .wasm file"));
 }
