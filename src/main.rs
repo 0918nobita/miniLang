bb80440dd48f59b169858e11e63c824a52d2a08c
0918nobita <nobita.0918@gmail.com>
2020-03-03 19:01:34 +0900

    :sparkles: .wasm ファイルを出力するようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate bincode;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use std::fs;
+
+#[derive(Serialize)]
+struct Header {
+    magic_cookie: u32,
+    version: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Psyche compiler 0.1.0");
+    let header = Header { magic_cookie: 0x6d736100, version: 1 };
+    let encoded = bincode::serialize(&header).unwrap();
+    fs::write("out.wasm", encoded).unwrap_or_else(|_| {
+        panic!("Failed to write .wasm file")
+    });
 }
