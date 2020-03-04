3c16a610acae4ba361b9e934eb281928631672d2
0918nobita <nobita.0918@gmail.com>
2020-03-04 20:59:08 +0900

    :building_construction: import section の内容を vector で表現するようにした
    serialize メソッドで Vec<u8> にシリアライズする

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,37 @@ mod type_section;
 
 use func_section::FuncSection;
 use header::Header;
-use import_section::ImportSection;
-use type_section::TypeSection;
+use import_section::{ImportContent, ImportKind, ImportSection};
+use type_section::{FuncType, PrimitiveType, TypeSection};
 
 fn main() {
     println!("Psyche compiler 0.1.0");
 
     let header: Header = Default::default();
-    let type_section: TypeSection = Default::default();
-    let import_section: ImportSection = Default::default();
+    let mut type_section: TypeSection = Default::default();
+    let mut import_section: ImportSection = Default::default();
     let func_section: FuncSection = Default::default();
+
+    let signature_index = type_section.add_type(&FuncType {
+        params: vec![
+            PrimitiveType::I32,
+            PrimitiveType::I32,
+            PrimitiveType::I32,
+            PrimitiveType::I32,
+        ],
+        result: Some(PrimitiveType::I32),
+    });
+
+    import_section.imports.push(ImportContent {
+        module: String::from("wasi_unstable"),
+        field: String::from("fd_write"),
+        kind: ImportKind::Function,
+        signature_index,
+    });
 
     let mut encoded_header = bincode::serialize(&header).unwrap();
     let mut encoded_type_section = type_section.serialize();
-    let mut encoded_import_section = bincode::serialize(&import_section).unwrap();
+    let mut encoded_import_section = import_section.serialize();
     let mut encoded_func_section = func_section.serialize();
 
     encoded_header.append(&mut encoded_type_section);
