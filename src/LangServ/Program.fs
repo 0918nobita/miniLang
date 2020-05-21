8e7c18dec94d5df38707b6fa0149b6fb657d5422
0918nobita <nobita.0918@gmail.com>
2020-05-21 15:56:17 +0900

    :art: Format code

--- src/LangServ/Program.fs
+++ src/LangServ/Program.fs
@@ -1,10 +1,15 @@
-﻿open System
+module Program
+
+open System
 
 [<EntryPoint>]
 let main argv =
     let reader = new IO.StreamReader(Console.OpenStandardInput())
+
     let writer = new IO.StreamWriter(Console.OpenStandardOutput())
+
     writer.AutoFlush <- true
+
     while true do
         let str = reader.ReadLine()
         writer.Write(str)
