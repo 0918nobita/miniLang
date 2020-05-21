803bdcc117f1d842e3a82c3518a5be8fb13bc0f8
0918nobita <nobita.0918@gmail.com>
2020-05-21 15:24:00 +0900

    :sparkles: LangServ プロジェクトを追加

---
+++ src/LangServ/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let reader = new IO.StreamReader(Console.OpenStandardInput())
+    let writer = new IO.StreamWriter(Console.OpenStandardOutput())
+    writer.AutoFlush <- true
+    while true do
+        let str = reader.ReadLine()
+        writer.Write(str)
+        writer.WriteLine()
+    0
