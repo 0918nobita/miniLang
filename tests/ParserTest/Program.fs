fcfc736e2dd7c1298208d1c43ead17e014353b29
0918nobita <nobita.0918@gmail.com>
2020-05-21 09:43:38 +0900

    :art: Format code

--- tests/ParserTest/Program.fs
+++ tests/ParserTest/Program.fs
@@ -1,22 +1,20 @@
-﻿module ParserTest
+module ParserTest
 
 open Expecto
 
 [<Tests>]
 let tests =
-    test "simple test"
-        { let subject = "Hello, world"
-          Expect.equal subject "Hello, world" "The strings should equal"
+    test "simple test" {
+        let subject = "Hello, world"
+        Expect.equal subject "Hello, world" "The strings should equal"
     }
 
 [<Tests>]
 let properties =
     testList "FsCheck"
-        [ testProperty "Addition is commutative"
-          <| fun a b -> a + b = b + a
+        [ testProperty "Addition is commutative" <| fun a b -> a + b = b + a
 
-          testProperty "reverse list"
-          <| fun (xs: int list) ->
+          testProperty "reverse list" <| fun (xs: int list) ->
               let reversed = List.rev xs
               List.rev reversed = xs ]
 
