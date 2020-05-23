8fa52b37b3d78ed3fa8a0aadd94c668c3d730239
0918nobita <nobita.0918@gmail.com>
2020-05-23 12:49:05 +0900

    :construction: Argu でコマンドライン引数をパースするようにした

--- src/Compiler/Program.fs
+++ src/Compiler/Program.fs
@@ -1,9 +1,43 @@
 module Program
 
 open Argu
-open FParsec
+open System
+
+type BuildArgs =
+    | [<AltCommandLine("-o")>] Out of string
+    | [<AltCommandLine("-v")>] Verbose
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Out _ -> "Specify output file"
+            | Verbose -> "Print a lot of output to stdout"
+
+type HelpArgs =
+    | [<CliPrefix(CliPrefix.None); Hidden>] Build
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Build -> ""
+
+type Arguments =
+    | [<CliPrefix(CliPrefix.None)>] Build of ParseResults<BuildArgs>
+    | [<CliPrefix(CliPrefix.None); Hidden>] Help of ParseResults<HelpArgs>
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Build -> "Compile packages and dependencies"
+            | Help -> ""
 
 [<EntryPoint>]
 let main argv =
-    printfn "Psyche Compiler"
+    let errorHandler =
+        ProcessExiter
+            (colorizer =
+                function
+                | ErrorCode.HelpText -> None
+                | _ -> Some ConsoleColor.Red)
+
+    let parser = ArgumentParser.Create<Arguments>(programName = "psy.exe", errorHandler = errorHandler)
+    let results = parser.ParseCommandLine argv
+    printfn "Results: %A" <| results.GetAllResults()
     0
