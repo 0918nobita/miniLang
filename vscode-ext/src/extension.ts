ed3290f8334f08ffb0b882a603b30e0d00bc41cf
0918nobita <nobita.0918@gmail.com>
2020-05-24 13:11:25 +0900

    :wrench: eslint, npm scripts の設定を変更

--- vscode-ext/src/extension.ts
+++ vscode-ext/src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('psycode.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('psycode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
@@ -24,4 +24,5 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function deactivate(): void {}
