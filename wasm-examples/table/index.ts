f8a8bfe12aa04e5eb28f6ce0ec83f2b4a73930a7
0918nobita <nobita.0918@gmail.com>
2020-05-26 11:43:40 +0900

    :art: Improve readability

--- wasm-examples/table/index.ts
+++ wasm-examples/table/index.ts
@@ -1,5 +1,9 @@
-const { instance: { exports: { add, call_indirect, tbl } } } =
+const { instance } =
     await WebAssembly.instantiate(Deno.readFileSync('table.wasm'), {});
+
+const call_indirect: (i: number) => number = instance.exports.call_indirect;
+const tbl: WebAssembly.Table = instance.exports.tbl;
+const add: (x: number, y: number) => number = instance.exports.add;
 
 console.log(call_indirect(0)); // => 111
 console.log(call_indirect(1)); // => 222
