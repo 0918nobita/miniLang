2c35b1e4dfed2a003ecc724a59f4aae3ac53b09d
0918nobita <nobita.0918@gmail.com>
2020-05-27 12:51:05 +0900

    :art: Improve readability

--- src/Compiler/Parser.fs
+++ src/Compiler/Parser.fs
@@ -3,9 +3,11 @@ module Parser
 open FParsec
 
 let identParser: Parser<string, unit> =
-    IdentifierOptions
-        (isAsciiIdStart = (fun c -> isAsciiLetter c || c = '_'),
-         isAsciiIdContinue = fun c ->
-             isAsciiLetter c || isDigit c || c = '_' || c = '\'') |> identifier
+    let options =
+        IdentifierOptions
+            (isAsciiIdStart = (fun c -> isAsciiLetter c || c = '_'),
+             isAsciiIdContinue =
+                 fun c -> isAsciiLetter c || isDigit c || c = '_' || c = '\'')
+    identifier options
 
 let runIdentParser() = printfn "%A" <| run identParser "_abc"
