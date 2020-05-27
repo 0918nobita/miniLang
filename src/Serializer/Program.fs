5c4c05f9d588dc5a6cd0bc009b5ddb143564656c
0918nobita <nobita.0918@gmail.com>
2020-05-27 15:38:04 +0900

    :sparkles: F# のレコードのフィールド一覧を取得するサンプルを追加

--- src/Serializer/Program.fs
+++ src/Serializer/Program.fs
@@ -1,6 +1,15 @@
 ﻿module Program
 
+open Microsoft.FSharp.Reflection
+
+type MyRecord =
+    { Foo: int
+      Bar: string }
+
 [<EntryPoint>]
 let main argv =
-    printfn "Serializer"
+    let schemaType = typeof<MyRecord>
+    let fields = FSharpType.GetRecordFields(schemaType)
+    printfn "[Fields]"
+    fields |> Seq.iter (fun field -> printfn "  %s: %A" field.Name field.PropertyType)
     0
